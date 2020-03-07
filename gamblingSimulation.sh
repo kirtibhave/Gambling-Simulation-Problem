a02d5377e80ca23b445cdf08ac276ae003ef82e1
kirtibhave <kirtibhave1000@gmail.com>
2020-03-07 19:09:47 +0530

    ADDED:Function to check gambler won or loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -5,4 +5,18 @@ echo "**********Welcome to Gambling Simulator Problem**********"
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
+#VARIABLES
+cash=$STAKE_PER_DAY
 
+#function is used to check win or loose condition
+function checkCondition(){
+RandomCheck=$(($RANDOM%2))
+	if [ 1 -eq $RandomCheck ]
+	then
+		cash=$STAKE_PER_DAY+$BET_PER_GAME
+	else
+		cash=$STAKE_PER_DAY-$BET_PER_GAME
+	fi
+}
+
+checkCondition
