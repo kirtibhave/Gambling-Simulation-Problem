db3e2ffd1374cc86671a345114661d8e328d22aa
kirtibhave <kirtibhave1000@gmail.com>
2020-03-08 14:57:32 +0530

    ADDED:Function to calculate gambler won or loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
+
 echo "**********Welcome to Gambling Simulator Problem**********"
 
 #CONSTANTS
@@ -8,24 +9,47 @@ BET_PER_GAME=1
 #VARIABLES
 cash=$STAKE_PER_DAY
 
+declare -A countMonths
+
+#function is used to calculate percentage
 function calculatePercentage(){
-   MAXIMUM_RESULT=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))  
-   MINIMUM_RESULT=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
+	MAXIMUM_RESULT=$(( (50*$STAKE_PER_DAY/100)+$STAKE_PER_DAY ))  
+	MINIMUM_RESULT=$(( (50*$STAKE_PER_DAY/100)-$STAKE_PER_DAY ))
 }
-calculatePercentage
 
+#function is used to calculate won or loose condition
 function calculateGambler(){
-while [[ $cash -gt $MINIMUM_RESULT && $cash -lt $MAXIMUM_RESULT ]]
-do
-RandomCheck=$(($RANDOM%2))
-	if [[ 1 -eq $RandomCheck ]]
-	then
-		cash=$(($cash+$BET_PER_GAME))
-	else
-		cash=$(($cash-$BET_PER_GAME))
-	fi
-	echo $cash
-done
+	while [[ $cash -gt $MINIMUM_RESULT && $cash -lt $MAXIMUM_RESULT ]]
+ 	do
+		RandomCheck=$((RANDOM%2))
+			if [ 1 -eq $RandomCheck ]
+			then
+				cash=$(($cash+$BET_PER_GAME))
+			else
+				cash=$(($cash-$BET_PER_GAME))
+			fi
+   done
+         totalCash=$cash-$STAKE_PER_DAY
+         echo $totalCash
 }
 
-calculateGambler
+#function is used to calculate total amount won or loose after 20 days
+function calculateTotalAmount(){
+for ((day=1;day<=2;day++))
+do
+	totalAmount=$((totalAmount + $(calculateGambler) ))
+	countMonths[day$day]=$totalAmount;
+done
+	echo "total amount is: $totalAmount"
+
+if [ $totalAmount -gt 0 ]
+then
+	echo "YOU WON,WOULD RESIGN FOR THE DAY"
+else
+	echo "YOU LOOSE,WOULD RESIGN FOR THE DAY"
+fi
+}
+
+calculatePercentage
+calculateTotalAmount
+
